c16a48e2fdba350daed1356e8827a822c4ba635b
HP <HP@LAPTOP-85HGS9R4>
2024-05-01 15:54:31 +0530

    Updated

---
+++ sampleprj/src/main/java/samplemaven1/SampleMavenProject.java
@@ -0,0 +1,19 @@
+package samplemaven1;
+
+public class SampleMavenProject {
+	public void display()
+	{
+	int a=10;
+	System.out.println("Value of a="+a);
+		
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SampleMavenProject sm=new SampleMavenProject();
+		sm.display();
+	}
+
+}
